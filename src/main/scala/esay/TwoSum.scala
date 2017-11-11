a8aadb4fca802072dd2baa97e4f55517ebfba738
liuziyang <616560426@qq.com>
2017-11-11 21:36:04 +0800

    commit

--- src/main/scala/esay/TwoSum.scala
+++ src/main/scala/esay/TwoSum.scala
@@ -1,24 +1,26 @@
 package esay
-
+import scala.util.control._
 /**
   * Created by Administrator on 2017/11/9.
   */
 object TwoSum {
+  val loop=new Breaks
   def twoSum(nums: Array[Int], target: Int): Array[Int] = {
     val map=scala.collection.mutable.Map[Int,Int]()
     for(i<-nums.indices){
      map(nums(i))=i
     }
     val result=scala.collection.mutable.Buffer[Int]()
+    loop.breakable{
     for(i<-nums.indices){
       val complement=target-nums(i)
       if(map.contains(complement)&&map(complement)!=i){
         println(i)
         result+=i
         result+=map(complement)
+        loop.break()
       }
-    }
-    println(map)
+    }}
     result.toArray
   }
 

9f48108d3a7e779d8be42a42e98812c06a0398c8
liuziyang <616560426@qq.com>
2017-11-12 22:38:31 +0800

    commit

--- src/main/scala/esay/FindLHS.scala
+++ src/main/scala/esay/FindLHS.scala
@@ -5,23 +5,24 @@ package esay
   */
 object FindLHS {
   def findLHS(nums: Array[Int]): Int = {
-val value2Count: Array[(Int, Int)] =nums.map((_,1)).groupBy(_._1)
-  .map{case (word,countIterator)=>(word,countIterator.length)}.toArray.sortBy(_._1)
-//    println(value2Count.mkString)
-//    result.foreach(line=>println(line._1,line._2.mkString))
-    val values2Count=scala.collection.mutable.HashMap[(Int,Int),Int]()
-for(i<-0 until value2Count.length-1){
-  if(value2Count(i)._1+1==value2Count(i+1)._1){
-    values2Count((value2Count(i)._1,value2Count(i+1)._1))=value2Count(i+1)._2+value2Count(i)._2
-  }
-}
-     values2Count.maxBy(_._2)._2
+    if(nums.length==0) return 0
+    val value2Count: Array[(Int, Int)] = nums.map((_, 1)).groupBy(_._1)
+      .map { case (word, countIterator) => (word, countIterator.length) }.toArray.sortBy(_._1)
+    val values2Count = scala.collection.mutable.HashMap[(Int, Int), Int]()
+    if (value2Count.length == 1) return 0
+    for (i <- 0 until value2Count.length - 1) {
+      if (value2Count(i)._1 + 1 == value2Count(i + 1)._1) {
+        values2Count((value2Count(i)._1, value2Count(i + 1)._1)) = value2Count(i + 1)._2 + value2Count(i)._2
+      }
+    }
+    if(values2Count.isEmpty) 0
+    else values2Count.maxBy(_._2)._2
 
 
   }
 
 
   def main(args: Array[String]): Unit = {
-    findLHS(Array(1,3,2,2,5,2,3,7))
+   println( findLHS(Array(1,1,1,1)))
   }
 }

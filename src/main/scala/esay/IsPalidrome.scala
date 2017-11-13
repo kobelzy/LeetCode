6804aa949800fcbeda08513cfa9de9b81df8faae
lzy <kobeliuziyang@gmail.com>
2017-11-13 19:08:22 +0800

    commit

---
+++ src/main/scala/esay/IsPalidrome.scala
@@ -0,0 +1,16 @@
+package esay
+
+/**
+  * Created by taihe on 2017/11/13.
+  */
+object No9_IsPalidrome {
+  def isPalidrome(x:Int):Boolean={
+    val str=x.toString.toBuffer
+    if(str(0).equals('-')) str.remove(0)
+    str.reverse==str
+  }
+
+  def main(args: Array[String]): Unit = {
+    print(isPalidrome(-2147447412))
+  }
+}

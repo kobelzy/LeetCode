e33700f4678498066b373bcae99d0e47250aa9e6
lzy <kobeliuziyang@gmail.com>
2017-11-14 20:05:48 +0800

    commoit

--- src/main/python/easy/No9_IsPalindrome.py
+++ src/main/python/easy/No9_IsPalindrome.py
@@ -3,9 +3,16 @@ def No9_isPalindrome(x):
     :type x: int
     :rtype: bool
     """
-    x=str(x)
-    if(x.startsWith('-')):
-        return True
+    if x < 0:
+        return False
+    copy, reverse = x, 0
+
+    while copy:
+        reverse *= 10
+        reverse += copy % 10
+        copy /= 10
+
+    return x == reverse
     # ret=[(len(x)-i) for i in range(len(x)+1)]
     # statuses=[[False]*len(x) for i in range(len(x))]
     # for i in range(len(x)-1,-1,-1):

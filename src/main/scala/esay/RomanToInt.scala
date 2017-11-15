011aeb3269549276368e7f85052d60aa47f0b618
lzy <kobeliuziyang@gmail.com>
2017-11-15 19:34:34 +0800

    commit

--- src/main/scala/esay/RomanToInt.scala
+++ src/main/scala/esay/RomanToInt.scala
@@ -4,16 +4,20 @@ package esay
   * Created by taihe on 2017/11/15.
   */
 object RomanToInt {
-  def romanToInt(s:String):Int={
-    var i=0
-    if(!s.contains(".")){ i=s.toInt}
-    else{
-     i=s.split("\\.")(0).toInt
+  def romanToInt(s:String):Int= {
+    var num = 0
+    val map = Map('M' -> 1000, 'D' -> 500, 'C' -> 100, 'L' -> 50, 'X' -> 10, 'V' -> 5, 'I' -> 1)
+    for (i <- 0 until s.length - 1) {
+      if (map(s(i)) > map(s(i + 1))) {
+        num -= map(s(i))
+      } else {
+        num += map(s(i))
+      }
     }
-    i
+      num += map(s(s.length - 1))
+    num
   }
-
   def main(args: Array[String]): Unit = {
-    println(romanToInt("3453543635433431432421"))
+    println(romanToInt("XXI"))
   }
 }

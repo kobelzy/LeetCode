b2e41d0209d9796d55427258c4dc2983b55cd48c
liuziyang <616560426@qq.com>
2017-11-16 21:59:11 +0800

    commit

---
+++ src/main/python/easy/No13_RomanToInteger.py
@@ -0,0 +1,9 @@
+def romanToInt(s):
+        roman={'M':1000,'D':500,'C':100,'L':50,'X':10,'V':5,'I':1}
+        z=0
+        for i in range(0,len(s)-1):
+            if roman[s[i]]<roman[s[i+1]]:
+                z-=roman[s[i]]
+            else:
+                z+=roman[s[i]]
+        return z+roman[s[-1]]

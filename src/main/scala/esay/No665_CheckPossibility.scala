901325b7f429c2ab53bb23ad6eafb65de6021282
lzy <kobeliuziyang@gmail.com>
2017-11-21 19:56:49 +0800

    commit

--- src/main/scala/esay/No665_CheckPossibility.scala
+++ src/main/scala/esay/No665_CheckPossibility.scala
@@ -19,12 +19,16 @@ def checkPossibility(nums: Array[Int]): Boolean = {
 //false
   val buffer=nums.toBuffer
   var index=0
-  for(i<-1 until buffer.length){
-    if(buffer(i-1)>buffer(i)){
+  var i=1
+  while(i<buffer.length|| index<=1){
+
+    if(buffer(i-1)>buffer(i) ||index>1){
       index +=1
       if (i - 2 < 0 || nums(i - 2) <= nums(i)) nums(i - 1) = nums(i)
       else nums(i ) = nums(i-1)
+
     }
+    i+=1
   }
   index<=1
 }

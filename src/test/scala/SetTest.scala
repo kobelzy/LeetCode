9241ae94ef126b151a043bd7bfad26e313b2c496
liuziyang <616560426@qq.com>
2017-11-23 22:40:32 +0800

    commit

--- src/test/scala/SetTest.scala
+++ src/test/scala/SetTest.scala
@@ -4,5 +4,8 @@
 object SetTest {
   def main(args: Array[String]): Unit = {
     println(Set(1,2,1))
+    val arr=Array("","2","3","4")
+    println(arr..diff(Array("2",3,4)).mkString(","))
+    println(arr.mkString(","))
   }
 }

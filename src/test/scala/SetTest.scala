05a50de2bb5cfb4ad19af7c38f95ee761534462c
liuziyang <616560426@qq.com>
2017-11-23 23:10:15 +0800

    commit

--- src/test/scala/SetTest.scala
+++ src/test/scala/SetTest.scala
@@ -3,9 +3,14 @@
   */
 object SetTest {
   def main(args: Array[String]): Unit = {
-    println(Set(1,2,1))
-    val arr=Array("","2","3","4")
-    println(arr..diff(Array("2",3,4)).mkString(","))
-    println(arr.mkString(","))
+//    println(Set(1,2,1))
+//    val arr=Array("","2","3","4")
+//    println(arr.diff(Array("2",3,4)).mkString(","))
+//    println(arr.mkString(","))
+
+    val arrs=scala.collection.mutable.ListBuffer(1,2,3,4)
+    arrs-=arrs.last
+    arrs-=arrs.last
+    println(arrs)
   }
 }

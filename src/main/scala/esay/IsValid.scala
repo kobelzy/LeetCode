419d2500e42395574a425d981a772c65851e94ab
liuziyang <616560426@qq.com>
2017-11-23 22:33:16 +0800

    commit

---
+++ src/main/scala/esay/IsValid.scala
@@ -0,0 +1,32 @@
+package esay
+
+/**
+  * Created by Administrator on 2017/11/23.
+  */
+object IsValid {
+//'(', ')', '{', '}', '[',']'
+
+  def isValid(s: String): Boolean = {
+    var flag = true
+    val symbols = Map[Char, Char]('(' -> ')', '{' -> '}', '[' -> ']')
+    val stringBySymbol: String = for (char <- s if char == '(' || char == ')' || char == '{' || char == '}' || char == '[' || char == ']') yield char
+    val list = scala.collection.mutable.ListBuffer[Char]()
+    for (symbol <- stringBySymbol) {
+      if (symbols.keySet.contains(symbol)) {
+        list += symbol
+      } else if (symbols.values.toSet.contains(symbol)) {
+        if (list.nonEmpty && symbols(list.last) == symbol) {
+          list.remove(list.size - 1)
+        } else {
+          flag = false
+        }
+      }
+    }
+    if (list.nonEmpty) flag = false
+    flag
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(isValid("{}"))
+  }
+}

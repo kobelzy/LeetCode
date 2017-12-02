dafb9e4944cce98c91fae6247d52180ee14d6c78
liuziyang <616560426@qq.com>
2017-12-02 23:47:54 +0800

    commit

---
+++ src/main/scala/esay/No53_LengthOfLastWord.scala
@@ -0,0 +1,18 @@
+package esay
+
+/**
+  * Created by Administrator on 2017/12/2.
+  */
+object No53_LengthOfLastWord {
+  def lengthOfLastWord(s: String): Int = {
+    if(s.isEmpty) return 0
+       s.split(' ') match {
+        case Array() =>0
+        case strs:Array[String]=>strs.last.length
+      }
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(lengthOfLastWord("1 2"))
+  }
+}

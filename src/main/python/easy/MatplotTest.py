0954bff05f6169bfd199dca7f1da02e97d563552
liuziyang <616560426@qq.com>
2018-05-08 23:00:21 +0800

    commit

--- src/main/python/easy/MatplotTest.py
+++ src/main/python/easy/MatplotTest.py
@@ -8,6 +8,17 @@ import numpy as np
 # data.shape
 fig = plt.figure()
 ax1 = fig.add_subplot(2, 2, 1)
+ax2 = fig.add_subplot(2, 2, 2)
+# plt.subplots_adjust(0, 0)
 # ax1.plot(randn(1000).cumsum(), 'k')
-plt.plot([1.5, 3.5, -2, 1.5])
+ax2.hist([1.5, 3.5, -2, 1.5],color='k', alpha=0.3)
+x = np.linspace(-np.pi, np.pi, 100)
+ax1.plot(x, np.sin(x),'--')
 
+x2 = np.arange(1,10)
+y = x2
+ax3 = fig.add_subplot(223)
+# ax3.scatter([x2], [y],  facecolon = 'o')
+plt.scatter(x2, y, marker='o')
+print('通过')
+plt.show()

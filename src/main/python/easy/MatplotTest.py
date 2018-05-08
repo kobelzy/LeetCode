b5cd5f49eb1c7185836f6a9df22633fd4bb3c1fe
liuziyang <616560426@qq.com>
2018-05-08 22:19:16 +0800

    commit

---
+++ src/main/python/easy/MatplotTest.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+# import numpy.random.randm as randn
+#
+# data = pd.read_csv('E:\交通数据集\卡口识别.csv')
+# data.shape
+fig = plt.figure()
+ax1 = fig.add_subplot(2, 2, 1)
+# ax1.plot(randn(1000).cumsum(), 'k')
+plt.plot([1.5, 3.5, -2, 1.5])
+

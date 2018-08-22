3153c19b8afb9dade2d8e41b6a804bde5643f35e
liuziyang <616560426@qq.com>
2018-08-22 23:00:43 +0800

    '修改了具体重叠算法'

--- src/main/scala/easy/No850_Rectangle_Area2.scala
+++ src/main/scala/easy/No850_Rectangle_Area2.scala
@@ -65,6 +65,12 @@ object No850_Rectangle_Area2 {
   }
 //  0,2,0,3,0,1    0,1,2,3
 
+  def isTuple2HasEqueals(t1:(Int,Int),t2:(Int,Int))={
+    if(t1._1>t2._1) {if(t1._1<t2._2&& t1._2>t2._2) (t1._1,t2._2) else if(t1._2<t2._2)(t1._1,t1._2)}
+    else if(t1._1<t2._1){if(t1._2<t2._2 && t1._2>t2._1) (t2._1,t1._2)else if(t1._2>t2._2) (t2._1,t2._2) }
+
+
+  }
     def main(args: Array[String]): Unit = {
     val arr = Array(Array(0, 0, 2, 2), Array(1, 0, 2, 3), Array(1, 0, 3, 1))
     val result = rectangleArea2(arr)

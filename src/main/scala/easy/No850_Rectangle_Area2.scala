39aad76c667069124a98c1fc2d3ca36858599ed7
liuziyang <616560426@qq.com>
2018-09-01 11:42:38 +0800

    850题目第二种解法,取模

--- src/main/scala/easy/No850_Rectangle_Area2.scala
+++ src/main/scala/easy/No850_Rectangle_Area2.scala
@@ -7,8 +7,8 @@ import scala.collection.mutable.ListBuffer
   * 矩形面积2
   */
 object No850_Rectangle_Area2 {
-  def rectangleArea(rectangles: Array[Array[Int]]): Int = {
-    var distinceCellNums = 0
+  def rectangleArea(rectangles: Array[Array[Int]]): Long = {
+    var distinceCellNums = 0L
     var is_max = false
     for (i <- rectangles.indices) {
       for (j <- rectangles(i).indices) {
@@ -30,7 +30,7 @@ object No850_Rectangle_Area2 {
       })
       distinceCellNums = cell_arr.size
     } else {
-      distinceCellNums = 49
+      distinceCellNums = 49L
     }
     distinceCellNums
   }
@@ -41,9 +41,9 @@ object No850_Rectangle_Area2 {
     * @param rectangles
     * @return
     */
-  def rectangleArea2(rectangles: Array[Array[Int]]): Int = {
+  def rectangleArea2(rectangles: Array[Array[Int]]): Long = {
 
-    var distinceCellNums = 0
+    var distinceCellNums = 0L
 
     val max = math.pow(10, 9)
     val is_not_rather_max = rectangles.forall(_.forall(_ < max))
@@ -53,7 +53,7 @@ object No850_Rectangle_Area2 {
         .sorted
 //[),[),
       val ySlids=y_arr.sliding(2,1).filter(slid=>rectangles.filter(arr => arr(1) <= slid.head && arr(3) >= slid.last).map(x => (x(0), x(2))).nonEmpty)
-      val all: Iterator[Int] = ySlids.map(slid => {
+      val all: Iterator[Long] = ySlids.map(slid => {
         val y_min = slid.head
         val y_max = slid.last
                 println(s"--y_start:$y_min y_end:$y_max --")
@@ -66,8 +66,8 @@ object No850_Rectangle_Area2 {
           distinct_XTuple = updateRange(distinct_XTuple, x)
         })
 
-        val x_sum = distinct_XTuple.map(x => x._2 - x._1).sum
-        val y_sum = y_max - y_min
+        val x_sum:Long = distinct_XTuple.map(x => x._2 - x._1).sum
+        val y_sum:Long = y_max - y_min
         println(s"+++++++x_sum:$x_sum y_sum:$y_sum --")
 
         x_sum * y_sum
@@ -75,7 +75,7 @@ object No850_Rectangle_Area2 {
 
       distinceCellNums = all.sum
     } else {
-      distinceCellNums = 49
+      distinceCellNums = 49L
     }
     distinceCellNums
   }
@@ -94,7 +94,7 @@ object No850_Rectangle_Area2 {
 
       indexs += newRange
       result = arr.diff(indexs)
-      result += indexs.reduce(concat)
+      result += indexs.sortBy(_._1).reduce(concat)
     }else{
       result=result++arr
     }
@@ -126,17 +126,27 @@ object No850_Rectangle_Area2 {
 
 def testupdate()={
 
-
+  val all_xTuple=List((24,56), (62,92), (38,92))
+  var distinct_XTuple = ListBuffer[(Int, Int)]()
+  distinct_XTuple += all_xTuple.head
+  all_xTuple.tail.foreach(x => {
+    distinct_XTuple = updateRange(distinct_XTuple, x)
+  })
+  val x_sum = distinct_XTuple.map(x => x._2 - x._1).sum
+  println(s"+++++++x_sum:$x_sum ")
 
 }
   def main(args: Array[String]): Unit = {
 //    val arr = Array(Array(0, 0, 1, 1), Array(2, 2, 3, 3))
 //    val arr = Array(Array(0, 0, 1, 2), Array(1, 0, 2, 3), Array(1, 0, 3, 1))
 //    val arr = Array(Array(49,40,62,100), Array(11,83,31,99), Array(19,39,30,99)) //1584
-    val arr = Array(Array(24,40,56,75), Array(40,12,82,50), Array(62,44,92,67), Array(38,22,92,91)) //4636
+//    val arr = Array(Array(24,40,56,75), Array(40,12,82,50), Array(62,44,92,67), Array(38,22,92,91)) //4636
+    //862275791
+    val arr=Array(Array(224386961,128668997,546647847,318900555),Array(852286866,238086790,992627088,949888275),Array(160239672,137108804,398130330,944807066),Array(431047948,462092719,870611028,856851714),Array(736895365,511285772,906155231,721626624),Array(289309389,607009433,558359552,883664714),Array(780746435,397872372,931219192,863727103),Array(573523994,124874359,889018012,471879750),Array(619886375,149607927,727026507,446976526),Array(51739879,716225241,115331335,785850603),Array(171077223,267051983,548436248,349498903),Array(314437215,169054168,950814572,481179241),Array(64126215,646689712,595562376,829164135),Array(926011655,481539702,982179297,832455610),Array(40370235,231510218,770233582,851797196),Array(292546319,45032676,413358795,783606009),Array(424366277,369838051,453541063,777456024),Array(211837048,142665527,217366958,952362711),Array(228416869,402115549,672143142,644930626),Array(755018294,194555696,846854520,939022548),Array(192890972,586071668,992336688,759060552),Array(127869582,392855032,338983665,954245205),Array(665603955,208757599,767586006,276627875),Array(260384651,10960359,736299693,761411808),Array(46440611,559601039,911666265,904518674),Array(54013763,90331595,332153447,106222561),Array(73093292,378586103,423488105,826750366),Array(327100855,516514806,676134763,653520887),Array(930781786,407609872,960671631,510621750),Array(35479655,449171431,931212840,617916927))
+
     val result = rectangleArea2(arr)
     println(result)
-
+//    testupdate
     //    val set = scala.collection.mutable.Set[(Int, Int)]()
     //    val set2 = scala.collection.mutable.Set[(Int, Int)]()
     //    set.add((1, 1))

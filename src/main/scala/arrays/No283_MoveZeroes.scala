5e6bd93c52516bb73565696b3f0478ed8df44e0d
liuziyang <616560426@qq.com>
2018-09-10 22:42:27 +0800

    283解法

---
+++ src/main/scala/arrays/No283_MoveZeroes.scala
@@ -0,0 +1,29 @@
+package arrays
+
+/**
+  * Created by Administrator on 2018/9/10.
+  */
+object No283_MoveZeroes {
+  def moveZeroes(nums: Array[Int]) = {
+    var curIndex = nums.length - 1
+    var lastIndex = nums.length - 1
+    var count = 0
+
+    while (curIndex >= 0) {
+      if (nums(curIndex)== 0) {
+        count = lastIndex - curIndex
+        for(i<-0 to count)
+          nums(curIndex + i) = nums(curIndex + i + 1)
+        }
+        nums(lastIndex) = 0
+        lastIndex -= 1
+      }
+      curIndex -= 1
+
+  }
+
+  def main(args: Array[String]): Unit = {
+val arr=Array(0,1,0,3,12)
+    moveZeroes(arr)
+  }
+}

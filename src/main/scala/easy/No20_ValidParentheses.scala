c46c6a7ca3f4c046863390de378bfb6b502ae690
jdlzy <kobeliuziyang@gmail.com>
2018-10-21 22:12:10 +0800

    链表相关的测试题

---
+++ src/main/scala/easy/No20_ValidParentheses.scala
@@ -0,0 +1,15 @@
+package easy
+
+object No20_ValidParentheses {
+  def isValid(s: String): Boolean = {
+    val map = Map(')' -> '(', ']' -> '[', '}' -> '{')
+    val stack=scala.collection.mutable.Stack[Char]()
+    val keys=map.keys.toArray
+    for(c<-s){
+      if(!keys.contains(c)){
+        stack.push(c)
+      }
+
+    }
+  }
+}

858b3718fbf6c8df0f0a0c67e18935ad186c67fb
lzy <kobeliuziyang@gmail.com>
2018-11-13 18:53:08 +0800

    有一个超长字符串，总是报超时。。。

--- src/main/scala/medium/NO15_ThreeSum.scala
+++ src/main/scala/medium/NO15_ThreeSum.scala
@@ -2,46 +2,48 @@ package medium
 
 object NO15_ThreeSum {
 
-  def main(args: Array[String]): Unit = {
-    val nums = Array(-2, 0, 2, 2)
+    def main(args: Array[String]): Unit = {
+        //    val nums = Array(-2, 0, 2, 2)
+//        val nums = Array(0, -4, -1, -4, -2, -3, 2)
+val nums = Array(-1, 0, 1, 2, -1, -4)
 //    val nums = Array(-1, 0, 1, 2, -1, -4)
-    println(threeSum(nums))
-  }
+        println(threeSum(nums))
+    }
 
-  def threeSum(nums: Array[Int]): List[List[Int]] = {
-    import util.control.Breaks._
-    val result = scala.collection.mutable.ListBuffer[List[Int]]()
-    val sortNums = nums.sorted
-    breakable{
-    for (i <- 0 to sortNums.length - 2) {
-            if(i>0 && sortNums(i)==sortNums(i-1)) break()
-      var head = i + 1
-      var last = sortNums.length - 1
+    def threeSum(nums: Array[Int]): List[List[Int]] = {
+        import util.control.Breaks._
+        val result = scala.collection.mutable.ListBuffer[List[Int]]()
+        val sortNums = nums.sorted
+        for (i <- 0 to sortNums.length - 2) {
+            breakable {
+                if (i > 0 && sortNums(i) == sortNums(i - 1)) break()
 
-      /**
-        * 进行除了第一个元素外其他元素的循环，从两头向中间循环
-        */
-      while (head < last) {
-        val s = sortNums(i) + sortNums(head) + sortNums(last)
-        //如果三个数的和小于0，那么说明head元素比较小
-        if (s < 0) head += 1
-          //如果s大于0，说明结尾元素比较大，需要减小，取其前边的元素
-        else if (s > 0) last -= 1
-        else {
-          //否则结果刚好为0，
-          result += List(sortNums(i), sortNums(head), sortNums(last))
-          //继续做魂环，如果下一个头元素和当前相等，那么跳过下一个，
-          while (head < last && sortNums(head) == sortNums(head + 1)) head += 1
-          //如果下一个尾元素和当前相等，则跳过下一个
-          while (head < last&& nums(last) == nums(last - 1)) head -= 1
-          head += 1
-          last -= 1
+                var head = i + 1
+                var last = sortNums.length - 1
+
+                /**
+                  * 进行除了第一个元素外其他元素的循环，从两头向中间循环
+                  */
+                while (head < last) {
+                    val s = sortNums(i) + sortNums(head) + sortNums(last)
+                    //如果三个数的和小于0，那么说明head元素比较小
+                    if (s < 0) head += 1
+                    //如果s大于0，说明结尾元素比较大，需要减小，取其前边的元素
+                    else if (s > 0) last -= 1
+                    else {
+                        //否则结果刚好为0，
+                        result += List(sortNums(i), sortNums(head), sortNums(last))
+                        //继续做魂环，如果下一个头元素和当前相等，那么跳过下一个，
+                        while (head < last && sortNums(head) == sortNums(head + 1)) head += 1
+                        //如果下一个尾元素和当前相等，则跳过下一个
+                        while (head < last && sortNums(last) == sortNums(last - 1)) last -= 1
+                        head += 1
+                        last -= 1
+                    }
+
+                }
+            }
         }
-
-      }
-
+        result.toList
     }
-    }
-    result.toList
-  }
 }

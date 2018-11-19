2bdf4e1664d81143b19cf49dfc3f6599dc2b5866
lzy <kobeliuziyang@gmail.com>
2018-11-19 09:46:04 +0800

    二叉搜索树

--- src/main/scala/medium/No98_IsValidBST.scala
+++ src/main/scala/medium/No98_IsValidBST.scala
@@ -11,27 +11,33 @@ package medium
 object No98_IsValidBST {
 
     class TreeNode(var _value: Int) {
-          var value: Int = _value
-          var left: TreeNode = null
-          var right: TreeNode = null
-        }
+        var value: Int = _value
+        var left: TreeNode = null
+        var right: TreeNode = null
+    }
+
     def isValidBST(root: TreeNode): Boolean = {
-
-
+        isValid(root,root.left.value,root.right.value)
 
 
     }
-    def helper(root:TreeNode)={
-        if(root.value==null){
-            true
-        }
-        if(!helper(root.left)){
-            false
-        }
-        if()
+//    def helper(root:TreeNode)={
+//        if(root.value==null){
+//            true
+//        }
+//        if(!helper(root.left)){
+//            false
+//        }
+//        if()
+//
+//
+//            true
+//    }
 
-
-            true
+    def isValid(root: TreeNode, min: Int, max: Int): Boolean = {
+        if (root == null) return true
+        if (min != null && root.value <= min) return false
+        if (max != null && root.value >= max) return false
+        isValid(root.left, min, root.value) && isValid(root.right, root.value, max)
     }
-
 }

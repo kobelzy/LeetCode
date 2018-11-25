7f5797ddfee7e0637d3e59e1e31d244663b3c89f
jdlzy <kobeliuziyang@gmail.com>
2018-11-25 22:24:48 +0800

    判断二叉树的共同节点

---
+++ src/main/python/medium/No236_LowestCommonAncestorofaBinaryTree.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """

0c9a6eae21e78bf1fd16ace0ff7cbbcf281aa640
lzy <kobeliuziyang@gmail.com>
2018-11-27 09:14:32 +0800

    分治算法求x的n次方

---
+++ src/main/scala/medium/No50_Pow.scala
@@ -0,0 +1,24 @@
+package medium
+
+/**
+  * Auther: lzy
+  * Description:
+  * Date Created by： 9:12 on 2018/11/27
+  * Modified By：
+  */
+
+object No50_Pow {
+
+    /**
+      * 求x的n次方
+      * （1）直接调用math函数
+      * （2）傻乘法
+      * （3）分治算法
+      * @param x
+      * @param n
+      * @return
+      */
+    def myPow(x: Double, n: Int): Double = {
+
+    }
+}

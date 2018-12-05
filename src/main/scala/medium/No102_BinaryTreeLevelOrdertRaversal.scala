50b95ef9034eda256620582238a0ae472e2257ec
lzy <kobeliuziyang@gmail.com>
2018-12-05 10:00:19 +0800

    层次遍历二叉树，使用广度优先+队列

--- src/main/scala/medium/No102_BinaryTreeLevelOrdertRaversal.scala
+++ src/main/scala/medium/No102_BinaryTreeLevelOrdertRaversal.scala
@@ -1,5 +1,7 @@
 package medium
 
+import scala.collection.mutable.ListBuffer
+
 /**
   * Auther: lzy
   * Description:
@@ -25,11 +27,71 @@ package medium
   */
 
 object No102_BinaryTreeLevelOrdertRaversal {
+     class TreeNode(var _value: Int) {
+         var value: Int = _value
+         var left: TreeNode = null
+         var right: TreeNode = null
+     }
+//    def levelOrder(self,root):
+//        if not root: return []
+//        result=[]
+//        queue=collections.deque()
+//        queue.append(root)
+//        #visited=set(root)
+//        while queue:
+//                level_size=len(queue)
+//                current_level=[]
+//                for _ in range(level_size):
+//                        node=queue.popleft()
+//                        current_elvel.append(node.val)
+//                        if node.left: queue.append(node.left)
+//                        if node.right:queue.append(node.right)
+//                result.append(curent_level)
+    /***
+     * 功能实现:
+     *BFS广度优先
+     * Author: Lzy
+     * Date: 2018/12/5 9:10
+     * Param: [root]
+     * Return: scala.collection.immutable.List<scala.collection.immutable.List<java.lang.Object>>
+     */
     def levelOrder(root: TreeNode): List[List[Int]] = {
+        if(root==null) return Nil
+        val result=ListBuffer[List[Int]]()
+        val queue=scala.collection.mutable.Queue[TreeNode]()
+        queue.enqueue(root)
 
+        while (!queue .isEmpty){
+        val level_size=queue.size
+            val current_level=ListBuffer[Int]()
+
+            for (i<- queue.indices){
+                val node=queue.dequeue()
+                current_level+=(node.value)
+                if(node.left!=null) queue.enqueue(node.left)
+                if(node.right!=null) queue.enqueue(node.left)
+            result+=(current_level.toList)
+
+            }
+
+        }
+        result.toList
     }
 
+/***
+ * 功能实现:
+ *DFS深度优先
+ * Author: Lzy
+ * Date: 2018/12/5 9:12
+ * Param: [root]
+ * Return: scala.collection.immutable.List<scala.collection.immutable.List<java.lang.Object>>
+ */
     def levelOrder2(root: TreeNode): List[List[Int]] = {
+        Nil
+    }
+
+    def levelOrder3(root: TreeNode): List[List[Int]] = {
+        Nil
 
     }
 }

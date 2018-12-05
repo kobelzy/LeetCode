32e07d4245164671c51b2cb714e2dc607c6062fc
lzy <kobeliuziyang@gmail.com>
2018-12-05 09:10:36 +0800

    层次遍历二叉树

---
+++ src/main/scala/medium/No102_BinaryTreeLevelOrdertRaversal.scala
@@ -0,0 +1,35 @@
+package medium
+
+/**
+  * Auther: lzy
+  * Description:
+  * 给定一个二叉树，返回其按层次遍历的节点值。 （即逐层地，从左到右访问所有节点）。
+  *
+  * 例如:
+  * 给定二叉树: [3,9,20,null,null,15,7],
+  *
+  * 3
+  * / \
+  * 9  20
+  * /  \
+  * 15   7
+  * 返回其层次遍历结果：
+  *
+  * [
+  * [3],
+  * [9,20],
+  * [15,7]
+  * ]
+  * Date Created by： 9:09 on 2018/12/5
+  * Modified By：
+  */
+
+object No102_BinaryTreeLevelOrdertRaversal {
+    def levelOrder(root: TreeNode): List[List[Int]] = {
+
+    }
+
+    def levelOrder2(root: TreeNode): List[List[Int]] = {
+
+    }
+}

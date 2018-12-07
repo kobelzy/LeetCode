794c4d6b8ec91c6eaa587267aa24cb51cc01119e
lzy <kobeliuziyang@gmail.com>
2018-12-07 09:59:22 +0800

    层次遍历二叉树，使用广度优先+队列

--- src/main/scala/medium/No102_BinaryTreeLevelOrdertRaversal.scala
+++ src/main/scala/medium/No102_BinaryTreeLevelOrdertRaversal.scala
@@ -47,9 +47,20 @@ object No102_BinaryTreeLevelOrdertRaversal {
 //                        if node.left: queue.append(node.left)
 //                        if node.right:queue.append(node.right)
 //                result.append(curent_level)
+
+    def main(args: Array[String]): Unit = {
+        val root=new TreeNode(3)
+        val node20=new TreeNode(20)
+        node20.left=new TreeNode(15)
+        node20.right=new TreeNode(7)
+        root.left=new TreeNode(9)
+        root.right=node20
+        println(levelOrder(root))
+    }
     /***
      * 功能实现:
      *BFS广度优先
+      * 可变List728ms
      * Author: Lzy
      * Date: 2018/12/5 9:10
      * Param: [root]
@@ -57,23 +68,25 @@ object No102_BinaryTreeLevelOrdertRaversal {
      */
     def levelOrder(root: TreeNode): List[List[Int]] = {
         if(root==null) return Nil
-        val result=ListBuffer[List[Int]]()
+        val result=scala.collection.mutable.ListBuffer[List[Int]]()
+//        var result=List[List[Int]]()
         val queue=scala.collection.mutable.Queue[TreeNode]()
         queue.enqueue(root)
 
-        while (!queue .isEmpty){
-        val level_size=queue.size
-            val current_level=ListBuffer[Int]()
+        while (queue .nonEmpty){
+            val size=queue.size
+            val current_level=scala.collection.mutable.ListBuffer[Int]()
+//            var current_level=List[Int]()
 
-            for (i<- queue.indices){
+            for(i<- 0 until size){
                 val node=queue.dequeue()
                 current_level+=(node.value)
+//                current_level=current_level:+(node.value)
                 if(node.left!=null) queue.enqueue(node.left)
-                if(node.right!=null) queue.enqueue(node.left)
-            result+=(current_level.toList)
-
+                if(node.right!=null) queue.enqueue(node.right)
             }
-
+                result+=(current_level.toList)
+//                result=result:+(current_level)
         }
         result.toList
     }
@@ -92,6 +105,33 @@ object No102_BinaryTreeLevelOrdertRaversal {
 
     def levelOrder3(root: TreeNode): List[List[Int]] = {
         Nil
+
+    }
+
+    def levelOrder4(root: TreeNode): List[List[Int]] = {
+        var queue = collection.mutable.ListBuffer.empty[(TreeNode, Int)]
+        if (null == root) {
+            return List.empty
+        }
+        val result = collection.mutable.ListBuffer.empty[List[Int]]
+        queue += ((root, 0))
+
+        while(queue.nonEmpty) {
+            val level = queue.head._2
+            val (tuples, rest) = queue.partition(t => t._2 == level)
+            result += tuples.map(t => t._1.value).toList
+            queue = rest
+            tuples.foreach { t =>
+                if (t._1.left != null) {
+                    queue += ((t._1.left, level + 1))
+                }
+                if (t._1.right != null) {
+                    queue += ((t._1.right, level + 1))
+                }
+            }
+        }
+
+        result.toList
 
     }
 }

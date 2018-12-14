47a73378a807cd82a0d566429f888c5fc7f588b2
lzy <kobeliuziyang@gmail.com>
2018-12-14 09:42:46 +0800

    八皇后问题

--- src/main/scala/hard/No51_NQueens.scala
+++ src/main/scala/hard/No51_NQueens.scala
@@ -31,35 +31,55 @@ import scala.collection.mutable.ListBuffer
   */
 
 object No51_NQueens {
-
+//一个已经存在的结论是，pei斜线上，x+y都是一个固定值。na斜线上，x-y都是一个固定值
     def main(args: Array[String]): Unit = {
         //        val row=new Array[Char](10)
 //        Arrays.fill(row,'.')
-val row = Array.fill(10)('.')
-        row(5)='Q'
-        println(row.mkString(""))
+        println(solveNQueens(4))
     }
 
     def solveNQueens(n: Int): List[List[String]] = {
         val res = new ListBuffer[List[String]]()
-        helper(0,new Array[Boolean](n),new Array[Boolean](2*n),new Array[Boolean](2*n),List[String](),res)
+        helper(0,new Array[Boolean](n),new Array[Boolean](2*n),new Array[Boolean](2*n),new Array[String](n),res)
         res.toList
     }
 
-    def helper(r: Int, cols: Array[Boolean], d1: Array[Boolean], d2: Array[Boolean], board: List[String], res: ListBuffer[List[String]]):Unit = {
+    /***
+     * 功能实现:
+     *
+     * Author: Lzy
+     * Date: 2018/12/14 9:01
+     * Param: [r, cols, d1, d2, board, res]
+      * r:棋盘深度
+      *
+      *
+      * board：代表棋盘，每一个深度用一个String表示
+     * Return: void
+     */
+    def helper(r: Int, cols: Array[Boolean], d1: Array[Boolean], d2: Array[Boolean], board: Array[String], res: ListBuffer[List[String]]):Unit = {
+
         if (r == board.size) {
-            res += (board)
+            res += (board.toList)
         } else {
             for (c <- board.indices) {
+                //坐标值（c,r）相当于（x,y）坐标
+
+//                (y-x)，na斜线对应的固定值,范围是0-》2*board.size-1
                 val id1 = r - c + board.size
+                //(x+y),pie斜线对应的固定值，0-》2*board.size-1
                 val id2 = 2 * board.size - r - c - 1
+                //如果当前位置没有棋子 && na斜线没有被占用  && pie斜线没有被占用
                 if (!cols(c) && !d1(id1) && !d2(id2)) {
                     val row = Array.fill(board.size)('.')
+
+                    //为棋子被占用的棋局赋值
                     row(c)='Q'
                     board(r)=new String(row)
                     cols(c)=true
                     d1(id1)=true
                     d2(id2)=true
+
+                    //迭代下一层
                     helper(r+1,cols,d1,d2,board,res)
                     cols(c)=false
                     d1(id1)=false

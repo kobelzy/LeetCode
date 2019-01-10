f1b32e822eca68f40fc5d83e6c3e747ed3e80e21
lzy <kobeliuziyang@gmail.com>
2019-01-10 09:30:20 +0800

    并查集-Java版本路径压缩

---
+++ src/main/scala/sort/QuickUnionUF.java
@@ -0,0 +1,41 @@
+package sort;
+
+/**
+ * 并查集，路径压缩
+ * @program: LeetCode
+ * @description:
+ * @author: Lzy
+ * @create: 2019-01-10 09:25
+ **/
+public class QuickUnionUF {
+    private int[] roots;
+
+    public QuickUnionUF(int N){
+        roots=new int[N];
+        for(int i=0;i<N;i++){
+            roots[i]=i;
+        }
+    }
+
+    private int findRoot(int i){
+        int root =i;
+        while(root !=roots[root]) root =roots[root];
+        while (i!=roots[i]){
+            int tmp =roots[i];
+            roots[i]=root;
+            i=tmp;
+        }
+            return root;
+    }
+
+    public boolean connected(int p,int q){
+        return findRoot(p)==findRoot(q);
+    }
+
+    public void union(int p,int q ){
+        int qroot=findRoot(q);
+        int proot=findRoot(p);
+        roots[proot]=qroot;
+    }
+
+}

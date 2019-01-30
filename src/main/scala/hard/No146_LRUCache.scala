7dd94d804c323a584842627d95df0ecc4af3c805
lzy <kobeliuziyang@gmail.com>
2019-01-30 10:15:07 +0800

    LRUCache实现

--- src/main/scala/hard/No146_LRUCache.scala
+++ src/main/scala/hard/No146_LRUCache.scala
@@ -36,119 +36,140 @@ object No146_LRUCache {
     def apply(capacity: Int): No146_LRUCache = new No146_LRUCache(capacity)
 
     def main(args: Array[String]): Unit = {
-        val map=scala.collection.mutable.Map(1->11,2->22)
-        map-=(1)
-        map.remove(1)
-        println(map)
 
         val cache = new No146_LRUCache(2 /* 缓存容量 */)
         println(cache.get(1))
+        println("head:" + cache.head.key, cache.tail.key);
+
         println(cache.put(2, 1))
+        println("head:" + cache.head.key, cache.tail.key);
+
         println(cache.get(2))
+        println("head:" + cache.head.key, cache.tail.key);
 
 
         println(cache.put(1, 1))
+        println("head:" + cache.head.key, cache.tail.key);
+
         println(cache.put(2, 2))
-        print("head:"+cache.head.key,cache.tail.key);println(cache.hm.keys.toList)
+        println("head:" + cache.head.key, cache.tail.key);
+        println(cache.hm.keys.toList)
         println(cache.get(1))       // 返回  1
-        println("head:"+cache.head.key,cache.tail.key);println(cache.hm.keys.toList)
+        println("head:" + cache.head.key, cache.tail.key);
+        println(cache.hm.keys.toList)
         println(cache.put(3, 3))    // 该操作会使得密钥 2 作废
-        println("head:"+cache.head.key,cache.tail.key);println(cache.hm.keys.toList)
+        println("head:" + cache.head.key, cache.tail.key);
+        println(cache.hm.keys.toList)
         println(cache.get(2))       // 返回 -1 (未找到)
-        println("head:"+cache.head.key,cache.tail.key);println(cache.hm.keys.toList)
+        println("head:" + cache.head.key, cache.tail.key);
+        println(cache.hm.keys.toList)
         println(cache.put(4, 4))    // 该操作会使得密钥 1 作废
-        println("head:"+cache.head.key,cache.tail.key);println(cache.hm.keys.toList)
+        println("head:" + cache.head.key, cache.tail.key);
+        println(cache.hm.keys.toList)
         println(cache.get(1))       // 返回 -1 (未找到)
         println(cache.get(3))       // 返回  3
         println(cache.get(4))       // 返回  4
 
     }
 }
-    /*
-    维护了两种数据结构：
-    Map：用于数据查询与存储
-    双向List：用于维护node关系，经常查询的保持在头端，不常被查询的逐步剔除
-     */
-    case class Node(key: Int, var value: Int, var next: Node, var pre: Node)
-    class No146_LRUCache(_capacity: Int) {
-        var count = 2
-        var head = Node(0, 0, null, null)
-        var tail = Node(0, 0, null, head)
-        head.next = tail
-        val hm :mutable.Map[Int, Node]= scala.collection.mutable.Map(head.key -> head, tail.key -> tail)
 
-        def get(key: Int): Int = {
-            if (hm.contains(key)) {
-                val node = hm(key)
+/*
+维护了两种数据结构：
+Map：用于数据查询与存储
+双向List：用于维护node关系，经常查询的保持在头端，不常被查询的逐步剔除
+ */
+case class Node(key: Int, var value: Int, var next: Option[Node], var pre: Option[Node])
+
+class No146_LRUCache(_capacity: Int) {
+    var count = 2
+    var head = Node(0, 0, None, None)
+    var tail = Node(0, 0, None, Some(head))
+    head.next = Some(tail)
+    val hm: mutable.Map[Int, Node] = scala.collection.mutable.Map(head.key -> head, tail.key -> tail)
+
+    def get(key: Int): Int = {
+        if (hm.contains(key)) {
+            val node = hm(key)
+            detachNode(node)
+            insertToHead(node)
+            node.value
+        } else -1
+    }
+
+    def put(key: Int, value: Int) {
+        hm.get(key) match {
+            case Some(node) =>
+                node.value = value
                 detachNode(node)
                 insertToHead(node)
-                node.value
-            } else -1
-        }
-
-        def put(key: Int, value: Int) {
-            hm.get(key) match {
-                case Some(node) =>
-                    node.value = value
-                    detachNode(node)
-                    insertToHead(node)
-                case None =>
-                    val newNode = Node(key, value,null,null )
-                    insertToHead(newNode)
-                    hm += key -> newNode
-                    this.count += 1
-                    if (this.count >= _capacity) removeNode()
-            }
-        }
-
-        /** 将节点放到缓存 头部 */
-        def insertToHead(node: Node) = {
-            this.head.pre = node
-            node.next = this.head
-            node.pre = null
-            this.head = node
-        }
-
-        /** 删除指定node */
-        def detachNode(node: Node) = {
-            if (node.next != null) {
-                node.pre.next = node.next //node的前继节点直接和node的后继节点相连
-                node.next.pre = node.pre
-            } else {
-                node.pre.next=null
-                this.tail = node.pre
-            }
-        }
-
-        /** 删除最后一个节点 */
-        def removeNode() = {
-            val tailKey = this.tail.key
-            hm -=tailKey
-             tail = this.tail.pre
-            tail.next = null
-            count -= 1
-        }
-
-
-
-        def print = {
-            println("\nPRINT CACHE ------ ")
-            println("count: " + count)
-            println("From head:")
-            var p = this.head
-
-            while (p != null) {
-                println("key: " + p.key + " value: " + p.value)
-                p = p.next
-            }
-            println("From tail:")
-            p = this.tail
-            while (p != null) {
-                println("key: " + p.key + " value: " + p.value)
-                p = p.pre
-            }
+            case None =>
+                val newNode = Node(key, value, None, None)
+                insertToHead(newNode)
+                hm += key -> newNode
+                this.count += 1
+                if (this.count >= _capacity) removeNode()
         }
     }
+
+    /** 将节点放到缓存 头部 */
+    def insertToHead(node: Node) = {
+        this.head.pre = Some(node)
+        node.next = Some(this.head)
+        node.pre = None
+        this.head = node
+    }
+
+    /** 删除指定node */
+    def detachNode(node: Node) = {
+        if (node == this.head) {
+            this.head = node.next.get
+            node.next.get.pre = None
+        } else if (node == this.tail) {
+            this.tail = node.pre.get
+            node.pre.get.next = None
+        } else {
+            node.pre.get.next = node.next
+            node.next.get.pre = node.pre
+        }
+
+//        if (node.next.isDefined) {//如果节点的下一个节点不为空
+//            //如果节点的pre不为空，将上一个节点直接和下一个节点相连
+//            if (node.pre.isDefined) node.pre.get.next = node.next
+//            //节点的next和pre相连
+//            node.next.get.pre = node.pre
+//        } else {
+//            node.pre.get.next = None
+//        }
+    }
+
+    /** 删除最后一个节点 */
+    def removeNode() = {
+        val tailKey = this.tail.key
+        hm -= tailKey
+        this.tail = this.tail.pre.get
+        this.tail.next = None
+        count -= 1
+    }
+
+
+    def print = {
+        println("\nPRINT CACHE ------ ")
+        println("count: " + count)
+        println("From head:")
+        var p = this.head
+
+        while (p != null) {
+            println("key: " + p.key + " value: " + p.value)
+            p = p.next.get
+        }
+        println("From tail:")
+        p = this.tail
+        while (p != null) {
+            println("key: " + p.key + " value: " + p.value)
+            p = p.pre.get
+        }
+    }
+}
 
 
 

800afec83b1116222b241ef7808e87ae561539ad
lzy <kobeliuziyang@gmail.com>
2019-02-13 14:08:11 +0800

    数据流中的第K大元素，Java优先队列实现

---
+++ src/main/scala/easy/No703_KthLargestElementInAStream.java
@@ -0,0 +1,54 @@
+package easy;
+
+
+import java.util.PriorityQueue;
+
+/**
+ * 数据流中的第K大元素
+ * 设计一个找到数据流中第K大元素的类（class）。注意是排序后的第K大元素，不是第K个不同的元素。
+ * <p>
+ * 你的 KthLargest 类需要一个同时接收整数 k 和整数数组nums 的构造器，它包含数据流中的初始元素。每次调用 KthLargest.add，返回当前数据流中第K大的元素。
+ * <p>
+ * 示例:
+ * <p>
+ * int k = 3;
+ * int[] arr = [4,5,8,2];
+ * KthLargest kthLargest = new KthLargest(3, arr);
+ * kthLargest.add(3);   // returns 4
+ * kthLargest.add(5);   // returns 5
+ * kthLargest.add(10);  // returns 5
+ * kthLargest.add(9);   // returns 8
+ * kthLargest.add(4);   // returns 8
+ * 说明:
+ * 你可以假设 nums 的长度≥ k-1 且k ≥ 1。
+ *
+ * @program: LeetCode
+ * @description:
+ * @author: Lzy
+ * @create: 2019-02-13 13:48
+ **/
+public class No703_KthLargestElementInAStream {
+   final PriorityQueue<Integer> q;
+   final int k;
+    public No703_KthLargestElementInAStream(int k, int[] nums) {
+        this.k=k;
+        this.q=new PriorityQueue<>(k);
+        for(int n:nums){
+            add(n);
+        }
+    }
+
+    public int add(int val) {
+        if(q.size()<k){
+            //不满足k个值，直接添加
+            q.add(val);
+        }else if(q.peek()<val){
+            //如果堆尾元素小于目标值，说明应该添加该值
+            q.poll();//堆尾出队
+            q.add(val);
+        }
+        //返回堆尾，也就是第k大元素
+        return q.peek();
+    }
+
+}

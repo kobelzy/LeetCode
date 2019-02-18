f381faa9c01b33e234a47533828d5e96d6e83a33
lzy <kobeliuziyang@gmail.com>
2019-02-18 10:43:51 +0800

    235-求二叉搜索树中的公共父节点

---
+++ src/main/scala/easy/No235_LowestCommonAncestorofaBinarySearchTree.java
@@ -0,0 +1,74 @@
+package easy;
+
+/**
+ * 二叉搜索树的最近公共祖先
+ * <p>
+ * 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+ * <p>
+ * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ * <p>
+ * 例如，给定如下二叉搜索树:  root = [6,2,8,0,4,7,9,null,null,3,5]
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+ * 输出: 6
+ * 解释: 节点 2 和节点 8 的最近公共祖先是 6。
+ * 示例 2:
+ * <p>
+ * 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+ * 输出: 2
+ * 解释: 节点 2 和节点 4 的最近公共祖先是 2, 因为根据定义最近公共祖先节点可以为节点本身。
+ * <p>
+ * <p>
+ * 说明:
+ * <p>
+ * 所有节点的值都是唯一的。
+ * p、q 为不同节点且均存在于给定的二叉搜索树中。
+ *
+ * @program: LeetCode
+ * @description:
+ * @author: Lzy
+ * @create: 2019-02-18 09:11
+ **/
+public class No235_LowestCommonAncestorofaBinarySearchTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        //递归停止条件
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        //提柜函数调用
+        TreeNode left=lowestCommonAncestor(root.left, p, q);
+        TreeNode right=lowestCommonAncestor(root.right,p,q);
+        //判断递归
+        return left ==null ? right:(right==null ?left :root);
+    }
+
+    /**
+     * 功能实现:二叉搜索树的最近公共祖先
+     * Author: Lzy
+     * Date: 2019/2/18 9:36
+     * Param:
+     * Return:
+     */
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+     if(p.val < root.val &&q.val<root.val){return lowestCommonAncestor(root.left,p,q);}
+     if(p.val > root.val &&q.val>root.val){return lowestCommonAncestor(root.right,p,q);}
+     return root;
+    }
+}

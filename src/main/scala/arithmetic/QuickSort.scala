729c873fad962ec31e7fbb103da8e464660f157d
lzy <kobeliuziyang@gmail.com>
2019-02-22 09:55:26 +0800

    快排优化

--- src/main/scala/arithmetic/QuickSort.scala
+++ src/main/scala/arithmetic/QuickSort.scala
@@ -3,12 +3,17 @@ package arithmetic
 object QuickSort {
   def main(args: Array[String]): Unit = {
     val list=List(5,3,9,2,7,6,7,1,4,8,10)
-    println(quickSort(list))
-    println(quickSort(list.toArray).mkString(","))
+//    println(quickSort(list))
+//    println(quickSort(list.toArray).mkString(","))
+    val list2=List(1)
+//    println(quickSort(list2.toArray).mkString(","))
+    println(quickSort(list2).mkString(","))
+
   }
   def quickSort(list:List[Int]):List[Int]={
     list match {
       case Nil => Nil
+//      case head::Nil=>head::Nil
       case head::tail=>{
         val (left,right)=tail.partition(_<head)
         quickSort(left) ::: head :: quickSort(right)

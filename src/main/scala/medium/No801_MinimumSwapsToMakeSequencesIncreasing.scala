a91e77ea32ee768ff607a599e6bfe6a7ba4228c2
jdlzy <kobeliuziyang@gmail.com>
2019-04-08 21:54:56 +0800

    19. 删除链表的倒数第N个节点

--- src/main/scala/medium/No801_MinimumSwapsToMakeSequencesIncreasing.scala
+++ src/main/scala/medium/No801_MinimumSwapsToMakeSequencesIncreasing.scala
@@ -1,6 +1,6 @@
 package medium
 
-/**801. 使序列递增的最小交换次数
+/** 801. 使序列递增的最小交换次数
   * 我们有两个长度相等且不为空的整型数组 A 和 B 。
   *
   * 我们可以交换 A[i] 和 B[i] 的元素。注意这两个元素在各自的序列中应该处于相同的位置。
@@ -22,21 +22,58 @@ package medium
   * A[i], B[i] 均为 [0, 2000]区间内的整数。
   */
 object No801_MinimumSwapsToMakeSequencesIncreasing {
+  /**
+    * 在第i位有一下三种情况
+    * A(i-1) >=B(i) or B(i-1) >=A(i)
+    * A(i-1) >=A(i) or B(i-1) >=B(i)
+    * A(i-1) < A(i) or B(i-a) < B(i)
+    *
+    **/
   def main(args: Array[String]): Unit = {
-    val A=Array(1,3,5,4)
-    val B=Array(1,2,3,7)
+    val A = Array(1, 3, 5, 4)
+    val B = Array(1, 2, 3, 7)
+    println(minSwap2(A,B))
   }
+
   def minSwap(A: Array[Int], B: Array[Int]): Int = {
 
-var i=1
-    var j=1
-    while(i<=A.length && j<=B.length){
-      if(A(i-1)>A(i)){
-        if(B(i))
+    val swap = new Array[Int](A.length)
+    val notSwap = new Array[Int](A.length)
+    swap(0) = 1
+    for (i <- 1 until A.length) {
+      if (A(i - 1) >= B(i) || B(i - 1) >= A(i)) {
+        swap(i) = swap(i - 1) + 1
+        notSwap(i) = notSwap(i - 1)
+      } else if (A(i - 1) >= A(i) || B(i - 1) >= B(i)) {
+        swap(i) = notSwap(i - 1) + 1
+        notSwap(i) = swap(i - 1)
+      } else if (A(i - 1) < A(i) || B(i - 1) < B(i)) {
+        val temp = math.min(swap(i - 1), notSwap(i - 1))
+        notSwap(i) = temp
+        swap(i) = temp + 1
       }
     }
+     math.min(swap.last, notSwap.last)
+  }
 
 
-    0
+  def minSwap2(A: Array[Int], B: Array[Int]): Int = {
+
+    var swap = 1
+    var notSwap =0
+    for (i <- 1 until A.length) {
+      if (A(i - 1) >= B(i) || B(i - 1) >= A(i)) {
+        swap+=1
+      } else if (A(i - 1) >= A(i) || B(i - 1) >= B(i)) {
+        val temp=notSwap
+        notSwap = swap
+        swap = temp + 1
+      } else if (A(i - 1) < A(i) || B(i - 1) < B(i)) {
+        val temp = math.min(swap, notSwap)
+        notSwap= temp
+        swap = temp + 1
+      }
+    }
+    math.min(swap, notSwap)
   }
 }

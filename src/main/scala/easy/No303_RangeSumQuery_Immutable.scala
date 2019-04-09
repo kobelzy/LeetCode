10d8ec1a823358f2307182df79fdc789d43df019
jdlzy <kobeliuziyang@gmail.com>
2019-04-09 22:28:57 +0800

    303. 区域和检索 - 数组不可变  动态规划题

--- src/main/scala/easy/No303_RangeSumQuery_Immutable.scala
+++ src/main/scala/easy/No303_RangeSumQuery_Immutable.scala
@@ -20,16 +20,22 @@ package easy
 object No303_RangeSumQuery_Immutable {
   // TODO: 待处理
   def main(args: Array[String]): Unit = {
-    val nums=Array(-2, 0, 3, -5, 2, -1)
-  var obj = new NumArray(nums)
-    val i=0
-    val j=5
-  var param_1 = obj.sumRange(i,j)
+    //    val nums = Array(-2, 0, 3, -5, 2, -1)
+    val nums = Array.empty[Int]
+    var obj = new NumArray(nums)
+    val i = 2
+    val j = 5
+    var param_1 = obj.sumRange(i, j)
+    println(param_1)
   }
 }
+
+//1,2,3,4,5   (2,4) 7
 class NumArray(_nums: Array[Int]) {
+  lazy val sums = if (_nums.isEmpty) _nums else _nums.tail.scan(_nums.head)(_ + _)
+  def sumRange(i: Int, j: Int): Int = if (i == 0) sums(j) else sums(j) - sums(i - 1)
 
-  def sumRange(i: Int, j: Int): Int = {
-
-  }
 }
+
+
+

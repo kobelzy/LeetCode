928bd896e46436d5bb02a7a743da47c0e6909911
lzy <kobeliuziyang@gmail.com>
2019-05-17 14:24:48 +0800

    215. 数组中的第K个最大元素

--- src/main/scala/medium/No215_KthLargestElementinanrray.scala
+++ src/main/scala/medium/No215_KthLargestElementinanrray.scala
@@ -1,6 +1,6 @@
 package medium
 
-/**215. 数组中的第K个最大元素
+/** 215. 数组中的第K个最大元素
   * 在未排序的数组中找到第 k 个最大的元素。请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
   *
   * 示例 1:
@@ -18,23 +18,34 @@ package medium
 
 object No215_KthLargestElementinanrray {
     def main(args: Array[String]): Unit = {
-//        val arr=Array(3,2,1,5,6,4)
-        val arr=Array(3,3,3,3,4,3,3,3,3)
-        println(findKthLargest(arr,1))
-    }
-    def findKthLargest(nums: Array[Int], k: Int): Int = {
-        rs1(nums,k)
+        //        val arr=Array(3,2,1,5,6,4)
+        val arr = Array(1, 3, 2)
+//        val arr=Array(3,3,3,3,4,3,3,3,3)
+        println(findKthLargest(arr, 2))
     }
 
-
-    def rs1(arr:Array[Int],targetIndex:Int): Int = {
-        if(arr.length<=2 &&arr.head==arr.last) return arr.head
-        val (left,right)=arr.tail.partition(_>=arr.head)
-        if(left.length>=targetIndex)
-            return rs1(left,targetIndex)
+    def findKthLargest1(nums: Array[Int], k: Int): Int = {
+        if (nums.length <= 2 && nums.head == nums.last) return nums.head
+        val (left, right) = nums.tail.partition(_ >= nums.head)
+        if (left.length >= k)
+            return findKthLargest1(left, k)
         else
-            return rs1(right:+arr.head,targetIndex-left.length)
+            return findKthLargest1(right :+ nums.head, k - left.length)
     }
 
 
+    /**
+      * 该方案更快一些
+      * @param nums
+      * @param k
+      * @return
+      */
+    def findKthLargest(nums: Array[Int], k: Int): Int = {
+        val head = nums.head
+        val (leftAndHead,right)=nums.partition(_>=head)
+        val left=leftAndHead.filter(_!=head)
+       return  if (k <= left.length)  findKthLargest(left, k)
+        else if (k <= leftAndHead.length)  head
+        else findKthLargest(right,k-leftAndHead.length)
+    }
 }

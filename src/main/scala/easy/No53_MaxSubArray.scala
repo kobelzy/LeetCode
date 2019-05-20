a90df56aab660ff6750b8e692daf1152943b1659
jdlzy <kobeliuziyang@gmail.com>
2019-05-20 22:41:05 +0800

    53. 最大子序和

--- src/main/scala/easy/No53_MaxSubArray.scala
+++ src/main/scala/easy/No53_MaxSubArray.scala
@@ -1,28 +1,36 @@
 package easy
-import scala.util.control._
 /**
-  * Created by taihe on 2017/11/30.
+  * 53. 最大子序和
+  * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+  *
+  * 示例:
+  *
+  * 输入: [-2,1,-3,4,-1,2,1,-5,4],
+  * 输出: 6
+  * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+  * 进阶:
+  *
+  * 如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
   */
 object No53_MaxSubArray {
   def maxSubArray(nums: Array[Int]): Int = {
+    nums.foldLeft(0,nums.head){(sn,x)=>
+      val cur=if(sn._1 <0) x else sn._1 +x
+      (cur,math.max(cur,sn._2))
 
-      var max=nums(0)
-    if(nums.length==1) return max
-    else if(nums.isEmpty) return 0
-      val map=scala.collection.mutable.Map[Int,Array[Int]]()
+    }._2
 
-  for (len <- 1 to nums.length) {
-    for (i <- 0 to nums.length - len) {
-      val buffer = scala.collection.mutable.ArrayBuffer[Int]()
-      for (j <- i until i + len ) {
-        buffer.append(nums( j))
+    /**
+      * 这个更好理解
+      * @param nums
+      * @return
+      */
+    def maxSubArray2(nums: Array[Int]): Int = {
+      for(i<-1 until nums.length){
+        nums(i)=nums(i)+math.max(nums(i-1),0)
       }
-      max = math.max(max, buffer.sum)
-      map(max) = buffer.toArray
+      nums.max
     }
-
-}
-   map.keys.max
   }
 
   def main(args: Array[String]): Unit = {

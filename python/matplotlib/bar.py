e53fa72abacc56270a64ddf419ff63a8faeb14b7
jdlzy <kobeliuziyang@gmail.com>
2019-08-11 22:54:39 +0800

    python他可视化

---
+++ python/matplotlib/bar.py
@@ -0,0 +1,17 @@
+
+
+
+import matplotlib.pyplot as plt
+# import numpy as np
+# import matplotlib
+print("hello")
+# plt.style.use('dark_backgroud')
+# fig, ax = plt.subplots()
+#
+# # x = np.array([1, 2, 3, 4, 5, 4, 5])
+# x = [1, 2, 3, 4, 5, 4, 5]
+# y = x * x
+# plt.bar(x, y)
+# for a, b in zip(x, y):
+#     plt.text(a, b + 0.2, '%d' % b, ha='center', va='bottom', fotsize=20)
+# plt.show()

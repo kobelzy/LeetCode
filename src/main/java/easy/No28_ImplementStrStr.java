e2319b53bf83a1123526f1cdf187fcc7524d37cc
jdlzy <kobeliuziyang@gmail.com>
2020-05-19 22:40:03 +0800

    28.实现 strStr()

---
+++ src/main/java/easy/No28_ImplementStrStr.java
@@ -0,0 +1,45 @@
+package easy;
+
+/**
+ * 28. 实现 strStr()
+ * 实现 strStr() 函数。
+ *
+ * 给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
+ *
+ * 示例 1:
+ *
+ * 输入: haystack = "hello", needle = "ll"
+ * 输出: 2
+ * 示例 2:
+ *
+ * 输入: haystack = "aaaaa", needle = "bba"
+ * 输出: -1
+ * 说明:
+ *
+ * 当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+ *
+ * 对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与C语言的 strstr() 以及 Java的 indexOf() 定义相符。
+ */
+public class No28_ImplementStrStr {
+    public static void main(String[] args) {
+        No28_ImplementStrStr test=new No28_ImplementStrStr();
+        int res=test.strStr("hello","ll");
+                assert res==2;
+    }
+    public int strStr(String haystack, String needle) {
+        int haystackLen=haystack.length();
+        int needleLen=needle.length();
+        if(needleLen==0) return 0;
+        if(haystackLen<needleLen) return -1;
+        for(int i=0;i<haystackLen-needleLen;i++){
+            if(haystack.charAt(i)==needle.charAt(0)){
+                for(int j=0;j<needleLen;j++){
+                    if(haystack.charAt(i+j)==needle.charAt(j)){
+                        if(j==needleLen-1) return i;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+}

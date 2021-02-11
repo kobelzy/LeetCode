e51749ed8773c777ceb7b7126c702f7dab4d893a
刘子洋 <liuziyang@miaozhen.com>
2021-02-11 12:22:29 +0800

    116. 填充每个节点的下一个右侧节点指针

---
+++ src/main/java/binaryTree/No116_PopulatingNextRightPointersInEachNode.java
@@ -0,0 +1,106 @@
+package binaryTree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 116. 填充每个节点的下一个右侧节点指针
+ * 给定一个 完美二叉树 ，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+ * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+ * <p>
+ * 初始状态下，所有 next 指针都被设置为 NULL。
+ * <p>
+ * 进阶：
+ * <p>
+ * 你只能使用常量级额外空间。
+ * 使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+ * <p>
+ * 输入：root = [1,2,3,4,5,6,7]
+ * 输出：[1,#,2,3,#,4,5,6,7,#]
+ * 解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。序列化的输出按层序遍历排列，同一层节点由 next 指针连接，'#' 标志着每一层的结束。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 树中节点的数量少于 4096
+ * -1000 <= node.val <= 1000
+ */
+public class No116_PopulatingNextRightPointersInEachNode {
+
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+
+    /**
+     * 解法1：层次遍历
+     * @param root
+     * @return
+     */
+    public Node connect(Node root) {
+        if (root == null) return null;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Node cur = queue.poll();
+                if (i < size - 1) cur.next = queue.peek();
+                if (cur.left != null) queue.offer(cur.left);
+                if (cur.right != null) queue.offer(cur.right);
+            }
+        }
+        return root;
+
+    }
+
+    /**
+     * 解法2：使用已建立的 \text{next}next 指针
+     * @param root
+     * @return
+     */
+    public Node connect2(Node root) {
+        if (root == null)             return root;
+
+        // 从根节点开始
+        Node leftmost = root;
+
+        while (leftmost.left != null) {
+            // 遍历这一层节点组织成的链表，为下一层的节点更新 next 指针
+            Node head = leftmost;
+            while (head != null) {
+                // CONNECTION 1
+                head.left.next = head.right;
+                // CONNECTION 2
+                if (head.next != null) {
+                    head.right.next = head.next.left;
+                }
+
+                // 指针向后移动
+                head = head.next;
+            }
+
+            // 去下一层的最左的节点
+            leftmost = leftmost.left;
+        }
+
+        return root;
+    }
+
+}

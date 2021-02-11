544e11d701403fe795ddb4cab16ab85dba28c86b
刘子洋 <liuziyang@miaozhen.com>
2021-02-11 11:04:53 +0800

    236. 二叉树的最近公共祖先

---
+++ src/main/java/binaryTree/No236_LowestCommonAncestorofaBinaryTree.java
@@ -0,0 +1,38 @@
+package binaryTree;
+
+/**
+ * 236. 二叉树的最近公共祖先
+ * 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+ * <p>
+ * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个节点 p、q，最近公共祖先表示为一个节点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ * 输入：root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+ * 输出：3
+ * 解释：节点 5 和节点 1 的最近公共祖先是节点 3 。
+ * 示例 2：
+ * 输入：root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+ * 输出：5
+ * 解释：节点 5 和节点 4 的最近公共祖先是节点 5 。因为根据定义最近公共祖先节点可以为节点本身。
+ * 输入：root = [1,2], p = 1, q = 2
+ * 树中节点数目在范围 [2, 105] 内。
+ * -109 <= Node.val <= 109
+ * 所有 Node.val 互不相同 。
+ * p != q
+ * p 和 q 均存在于给定的二叉树中。
+ */
+public class No236_LowestCommonAncestorofaBinaryTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        /*找1，3
+                4
+             /   \
+            2     6
+           / \    / \
+          1   3  4   7
+         */
+        if (root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null) return right == null ? left : root;
+        else return right;
+//        return left == null ? right : right == null ? left : root;
+    }
+}

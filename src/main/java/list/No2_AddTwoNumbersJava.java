497e36b572c9c8bf14e1d452176cde5a2b03b158
kobelzy <kobeliuziyang@gmail.com>
2021-02-12 18:38:43 +0800

    [代码重构](master): 链表代码package修改

--- src/main/java/list/No2_AddTwoNumbersJava.java
+++ src/main/java/list/No2_AddTwoNumbersJava.java
@@ -1,4 +1,4 @@
-package medium;
+package list;
 
 /**
  * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
@@ -12,10 +12,7 @@ package medium;
  * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
  * 输出：7 -> 0 -> 8
  * 原因：342 + 465 = 807
- *
- * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/add-two-numbers
- * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
  */
 public class No2_AddTwoNumbersJava {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {

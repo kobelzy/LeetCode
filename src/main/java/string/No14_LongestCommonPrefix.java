53918b86bfa0588e90e6e3ecabf384b703946136
kobelzy <kobeliuziyang@gmail.com>
2021-02-17 10:48:18 +0800

    14. 最长公共前缀

--- src/main/java/string/No14_LongestCommonPrefix.java
+++ src/main/java/string/No14_LongestCommonPrefix.java
@@ -1,6 +1,4 @@
-package easy;
-
-import scala.Int;
+package string;
 
 /**
  * 14. 最长公共前缀
@@ -22,18 +20,52 @@ import scala.Int;
  * 所有输入只包含小写字母 a-z 。
  */
 public class No14_LongestCommonPrefix {
+    /**
+     * 暴力
+     * @param strs
+     * @return
+     */
     public String longestCommonPrefix(String[] strs) {
         if (strs == null || strs.length == 0) return "";
         String head = strs[0];
         for (int i = 0; i < head.length(); i++) {
-            Character a = head.charAt(i);
-            for (int j = 0; j < strs.length; j++) {
+            char a = head.charAt(i);
+            for (int j = 1; j < strs.length; j++) {
                 if (i < strs[j].length()) {
                     if (strs[j].charAt(i) != a) return head.substring(0, i);
                 } else return head.substring(0, i);
             }
         }
         return strs[0];
+    }
+
+    /**
+     * 二分算法
+     *
+     * @param strs
+     * @return
+     */
+    public String longestCommonPrefix3(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        int minLen = Integer.MAX_VALUE;
+        for (String str : strs) minLen = Math.min(str.length(), minLen);
+        if(minLen==0) return "";
+
+        int low = 0;
+        int high = minLen;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (isCommonPrefix(strs, mid)) low = mid + 1;
+            else high = mid-1;
+        }
+        return strs[0].substring(0,low + ((high - low) >> 1));
+    }
+
+    private boolean isCommonPrefix(String[] strs, int len) {
+        String prefix = strs[0].substring(0, len);
+        for (int i = 0; i < strs.length; i++)
+            if (!strs[i].startsWith(prefix)) return false;
+        return true;
     }
 
 
@@ -53,7 +85,6 @@ public class No14_LongestCommonPrefix {
         else {
             //注意这里，位运算的符号等级非常低
             int mid = start + ((end - start) >> 1);
-            System.out.println(start + "," + end + "," + mid);
             String left = longestCommonPrefix2(strs, start, mid);
             String right = longestCommonPrefix2(strs, mid + 1, end);
             return longestCommonPrefixFromTwo(left, right);
@@ -69,35 +100,6 @@ public class No14_LongestCommonPrefix {
         return left;
     }
 
-    /**
-     * 二分算法
-     *
-     * @param strs
-     * @return
-     */
-    public String longestCommonPrefix3(String[] strs) {
-        if (strs == null || strs.length == 0) return "";
-        int minLen = Integer.MAX_VALUE;
-        for (int i = 0; i < strs.length; i++) minLen = Math.min(strs[i].length(), minLen);
-        if(minLen==0) return "";
-
-        int low = 0;
-        int high = minLen;
-        while (low <= high) {
-            int mid = low + ((high - low) >> 1);
-            System.out.println(mid);
-            if (isCommonPrefix(strs, mid)) low = mid + 1;
-            else high = mid-1;
-        }
-        return strs[0].substring(0,low + ((high - low) >> 1));
-    }
-
-    private boolean isCommonPrefix(String[] strs, int len) {
-        String prefix = strs[0].substring(0, len);
-        for (int i = 0; i < strs.length; i++)
-            if (!strs[i].startsWith(prefix)) return false;
-        return true;
-    }
 
 
 

a88d301b124013330a6a4bb4667560bf2e11f932
kobelzy <kobeliuziyang@gmail.com>
2021-02-18 21:57:52 +0800

    167. 两数之和 II - 输入有序数组

---
+++ src/main/java/string/No167_TwoSumII_InputArrayIsSorted.java
@@ -0,0 +1,68 @@
+package string;
+
+/**
+ * 167. 两数之和 II - 输入有序数组
+ * 给定一个已按照 升序排列  的整数数组 numbers ，请你从数组中找出两个数满足相加之和等于目标数 target 。
+ * 函数应该以长度为 2 的整数数组的形式返回这两个数的下标值。numbers 的下标 从 1 开始计数 ，所以答案数组应当满足 1 <= answer[0] < answer[1] <= numbers.length 。
+ * 你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。
+ * 示例 1：
+ * 输入：numbers = [2,7,11,15], target = 9
+ * 输出：[1,2]
+ * 解释：2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2 。
+ * 示例 2：
+ * 输入：numbers = [2,3,4], target = 6
+ * 输出：[1,3]
+ * 示例 3：
+ * <p>
+ * 输入：numbers = [-1,0], target = -1
+ * 输出：[1,2]
+ * 提示：
+ * <p>
+ * 2 <= numbers.length <= 3 * 104
+ * -1000 <= numbers[i] <= 1000
+ * numbers 按 递增顺序 排列
+ * -1000 <= target <= 1000
+ * 仅存在一个有效答案
+ */
+public class No167_TwoSumII_InputArrayIsSorted {
+    /**
+     * 双指针
+     *
+     * @param numbers
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) return new int[]{left + 1, right + 1};
+            else if (sum < target) left++;
+            else right--;
+        }
+        return new int[]{-1, -1};
+    }
+
+    /**
+     * 二分查找
+     *
+     * @param numbers
+     * @param target
+     * @return
+     */
+    public int[] twoSum2(int[] numbers, int target) {
+        for (int i = 0; i < numbers.length; i++) {
+            int num = target - numbers[i];
+            int left = i + 1;
+            int right = numbers.length - 1;
+            while (left <= right) {
+                int mid = left + ((right - left) >> 1);
+                if (numbers[mid] == num) return new int[]{i+1, mid+1};
+                else if (numbers[mid] > num) right = mid - 1;
+                else left = mid + 1;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}

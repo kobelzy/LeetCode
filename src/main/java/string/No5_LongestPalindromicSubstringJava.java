1bf929996922cb69558a6284fe44f1bb6dd09925
kobelzy <kobeliuziyang@gmail.com>
2021-02-19 11:34:13 +0800

    5.最长回文子串

---
+++ src/main/java/string/No5_LongestPalindromicSubstringJava.java
@@ -0,0 +1,79 @@
+package string;
+
+/**
+ * 5.最长回文子串
+ * 给你一个字符串 s，找到 s 中最长的回文子串。
+ * 示例 1：
+ * 输入：s = "babad"
+ * 输出："bab"
+ * 解释："aba" 同样是符合题意的答案。
+ * 示例 2：
+ *
+ * 输入：s = "cbbd"
+ * 输出："bb"
+ * 示例 3：
+ *
+ * 输入：s = "a"
+ * 输出："a"
+ * 示例 4：
+ * 输入：s = "ac"
+ * 输出："a"
+ * 提示：
+ * 1 <= s.length <= 1000
+ * s 仅由数字和英文字母（大写和/或小写）组成
+ */
+public class No5_LongestPalindromicSubstringJava {
+
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 2) return s;
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - ((len - 1) >> 1);
+                end = i + (len >> 1);
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+
+    /**
+     * 动态规划
+     *
+     * @param s
+     * @return
+     */
+    public String longestPalindrome2(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        String ans = "";
+        for (int l = 0; l < n; ++l) {
+            for (int i = 0; i + l < n; ++i) {
+                int j = i + l;
+                if (l == 0) {
+                    dp[i][j] = true;
+                } else if (l == 1) {
+                    dp[i][j] = s.charAt(i) == s.charAt(j);
+                } else {
+                    dp[i][j] = (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]);
+                }
+                if (dp[i][j] && l + 1 > ans.length()) {
+                    ans = s.substring(i, i + l + 1);
+                }
+            }
+        }
+        return ans;
+    }
+
+
+}

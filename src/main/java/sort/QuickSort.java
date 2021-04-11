410af9b99fa04d5ccc8bee6c9db332f44ad56e9e
kobelzy <kobeliuziyang@gmail.com>
2021-04-11 11:42:16 +0800

    [代码完善](master): 快速排序

--- src/main/java/sort/QuickSort.java
+++ src/main/java/sort/QuickSort.java
@@ -6,26 +6,31 @@ public class QuickSort {
 
     public static void main(String[] args) {
         QuickSort t=new QuickSort();
-        int[] nums={2,3,4,1};
+        int[] nums={9,8,7,6,0,4,3,2,1};
         t.quickSort(nums,0,nums.length-1);
         System.out.println(Arrays.toString(nums));
     }
     private void quickSort(int[] nums, int left, int right) {
         if (left >= right) return;
-        int lo = left+1;               // 小于分界点元素的最右侧的指针
-        int hi = right;                // 大于分界点元素的最左侧的指针
-        while (lo<=hi) {
-            if (nums[lo]>nums[left]) { // 交换元素确保左侧指针指向元素小于分界点元素
-                swap(nums, lo, hi);
-                hi--;
+        int i = left+1;               // 小于分界点元素的最右侧的指针
+        int j = right;                // 大于分界点元素的最左侧的指针
+        while (i<=j) {
+            if (nums[i]>nums[left]) { // 交换元素确保左侧指针指向元素小于分界点元素
+                swap(nums, i, j);
+                j--;
             } else {
-                lo++;
+                i++;
             }
         }
-        lo--;                          // 回到小于分界点元素数组的最右侧
-        swap(nums, left, lo);          // 将分界点元素移到左侧数组最右侧
-        quickSort(nums, left, lo-1);
-        quickSort(nums, lo+1, right);
+        /**
+         * 这个时候i>=nums[left]，可能等于，也可能是刚大于nums[left]第一个值
+         * i--正好取得nums[left]的最终排序位置
+         * 将nums[left]的值换到i，并各自进行内部的排序即可
+         */
+        i--;                          // 回到小于分界点元素数组的最右侧
+        swap(nums, left, i);          // 将分界点元素移到左侧数组最右侧
+        quickSort(nums, left, i-1);
+        quickSort(nums, i+1, right);
     }
 
 
@@ -33,5 +38,26 @@ public class QuickSort {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
+    }
+
+
+    public void qucikSort2(int[] nums,int left,int right){
+        if(left<right){
+            int i=partition(nums,left,right);
+            qucikSort2(nums,left,i-1);
+            qucikSort2(nums,i+1,right);
+        }
+    }
+    private int partition(int[] nums,int left,int right){
+        int i=left,j=right+1;
+        int temp=nums[left];
+        while(true){
+            while(nums[++i]<temp) if(i==right) break;
+            while(nums[--j]>temp) if(j==left) break;
+            if(i>=j) break;
+            swap(nums,i,j);
+        }
+        swap(nums,left,j);
+        return j;
     }
 }

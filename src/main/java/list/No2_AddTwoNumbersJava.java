5b30295875cfe6b3e882122305bf60eb4ed40e55
jdlzy <kobeliuziyang@gmail.com>
2021-04-25 22:58:10 +0800

    [代码完善](master): 2.两数相加

--- src/main/java/list/No2_AddTwoNumbersJava.java
+++ src/main/java/list/No2_AddTwoNumbersJava.java
@@ -1,6 +1,7 @@
 package list;
 
 /**
+ * 2.两数相加
  * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
  *
  * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
@@ -15,6 +16,21 @@ package list;
 
  */
 public class No2_AddTwoNumbersJava {
+
+    public ListNode addTwoNumbers2(ListNode l1,ListNode l2){
+        int flag=0;
+        ListNode preHead=new ListNode(-1),cur=preHead;
+        while(l1!=null || l2 !=null || flag!=0){
+            int sum=(l1==null ?0:l1.val)+(l2==null?0:l2.val)+flag;
+            flag=sum/10;
+            ListNode next=new ListNode(sum);
+            cur.next=next;
+            cur=cur.next;
+            l1=l1==null?null:l1.next;
+            l2=l2==null?null:l2.next;
+        }
+        return preHead.next;
+    }
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode preHead=new ListNode(0);
         ListNode cur=preHead;
@@ -39,4 +55,6 @@ public class No2_AddTwoNumbersJava {
         if(flag!=0) cur.next=new ListNode(flag);
         return preHead.next;
     }
+
+
 }
